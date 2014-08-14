2049d9c99a436bfa4c8ab205d5618fa0fda59da9
liuhaibo <haiboliu86@gmail.com>
2014-08-14 20:20:16 +0800

    My Fourth submit!

--- src/com/lhb/java/Main.java
+++ src/com/lhb/java/Main.java
@@ -6,14 +6,14 @@ public class Main {
      * @param args 参数
      */
     public static void main(String[] args) {
-	    double[] w = {7,2,1,5};
-        double[] v = {42,12,40,25};
+	    double[] w = {7,2,3,5};
+        double[] v = {42,16,40,25};
         double maxW = 10;
 
     //    BagSolveInf bagSolve = new GreedyBag();
     //    int[] res = bagSolve.solve(w,v,maxW);
 
-        GreedyContext greedyContext = new GreedyContext(new ValueGreedyBag());
+        GreedyContext greedyContext = new GreedyContext(new PreValueGreedyBag());
         int[] res = greedyContext.getResult(w,v,maxW);
 
         double maxV = 0;
